15da856b4f883d9b3de6149584471ff50d12d6e1
shihaoran <shihaoran.code@gmail.com>
2024-12-01 23:05:03 +0800

    feat:支持 markdown 渲染

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { marked } from "marked";
 
+import aiImage from '@/assets/imgs/ai.jpg'
 import { selectContent } from "@/store/modules/content";
 import { useStartConversation } from "@/service/index";
 import { setLoading } from "@/store/modules/loading";
@@ -9,6 +11,7 @@ const Main: React.FC = () => {
     const content = useSelector(selectContent)
     const dispatch = useDispatch()
     const { msg, response, follow } = content
+    const markResponse = marked(response)
     const startConversation = useStartConversation()
     const handleClick = async (item: string) => {
         dispatch(setLoading(true))
@@ -28,14 +31,15 @@ const Main: React.FC = () => {
                         {msg}
                     </div>
                 }
-                {response &&
-                    <div className="ai">
-                        {response}
+                {markResponse &&
+                    <div className="ai" >
+                        <img src={aiImage} alt="" />
+                        <div className="test" dangerouslySetInnerHTML={{ __html: markResponse }} />
                     </div>
                 }
-                {follow && follow.map((item, index) => (
+                {follow.length > 0 && follow.map((item, index) => (
                     <div className="follow_up" key={index} onClick={() => handleClick(item)}>
-                        <p>{item}</p>
+                        <p><a href="#">{item}</a></p>
                     </div>
                 ))}
             </div>
